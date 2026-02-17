83f42117362592df4ba2351d30b71ded128ec489
itzgregory <gregorychuks0@gmail.com>
2026-02-17 16:30:01 +0100

    feat: client foundation

--- server/src/TaskHub.Api/Controllers/WeatherForecastController.cs
+++ server/src/TaskHub.Api/Controllers/WeatherForecastController.cs
@@ -6,10 +6,10 @@ namespace TaskHub.Api.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries =
-    [
+    private static readonly string[] Summaries = new[]
+    {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    ];
+    };
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
